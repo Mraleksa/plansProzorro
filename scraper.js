506d82074883929f64d810805cf2da1bedf34ee2
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-13 19:01:19 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,59 +1,151 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
-
-var cheerio = require("cheerio");
-var request = require("request");
+var client = require('http-api-client');
 var sqlite3 = require("sqlite3").verbose();
+const sqliteJSON = require('sqlite-json');
+var d3 = require("d3");
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
+// Open a database handle
+var db = new sqlite3.Database("data.sqlite");
+
+	
+var currentCount =  "2017-03-25T00:00:00.008329+03:00"
+var p=0; var p2=0;
+   
+   
+function piv(){  
+p++;
+client.request({url: 'https://public.api.openprocurement.org/api/2.3/plans?offset='+currentCount})
+		.then(function (data) {
+						 
+		
+			var dataset = data.getJSON().data;
+			
+			currentCount = data.getJSON().next_page.offset;			
+			console.log(currentCount)
+			
+			return dataset;
+	
+		})	
+		.then(function (dataset) {	
+		
+			dataset.forEach(function(item) {
+				client.request({url: 'https://public.api.openprocurement.org/api/0/plans/'+item.id})
+					.then(function (data) {
+			
+				
+					
+db.serialize(function() {
+
+  // Create new table
+  db.run("CREATE TABLE IF NOT EXISTS data (id TEXT,datePublished TEXT,cpv TEXT,nameId TEXT,name TEXT,amount INT,currency TEXT,procurementMethod TEXT,procurementMethodType TEXT,startDate TEXT)");
+
+  
+  // Insert a new record
+  var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?,?)");
+
+statement.run(data.getJSON().data.id,data.getJSON().data.datePublished,data.getJSON().data.classification.id,data.getJSON().data.procuringEntity.identifier.id,data.getJSON().data.procuringEntity.name,data.getJSON().data.budget.amount,data.getJSON().data.budget.currency,data.getJSON().data.tender.procurementMethod,data.getJSON().data.tender.procurementMethodType,data.getJSON().data.tender.tenderPeriod.startDate);
+  //else none;
+   //console.log(item.dateModified)
+  statement.finalize();
+});
+
+					})
+					.catch(function  (error) {
+						console.log("error_detale")
+						
+					});  
+				});
+		
+		})
+		.then(function () {	
+		if (p<10){piv ();}		
+		else {
+			console.log("stop")
+				p=0;
+				p2++;
+				console.log(p2)
+			setTimeout(function() {
+			
+				if (p2 < 1) {
+					piv ();
+				}
+				else {
+					
+///////////////////////////////		
+
+db.run("DELETE FROM data2");					
+						
+const exporter = sqliteJSON(db);
+					
+exporter.json('SELECT * FROM data', function (err, json) {
+						
+						var nest=d3.nest()
+  						  .key(function(d) {return d.name;})
+						  .key(function(d) {return d.procurementMethod;})
+  						  .sortKeys(d3.ascending)
+  						  .rollup(function(v) { return {
+    							 count: v.length, 
+							total: d3.sum(v, function(d) { return d.amount; })
+  						   }; })
+  						  .entries(JSON.parse(json.replace(/limited/g, "open")));
+						
+
+
+	
+					
+nest.forEach(function(item) {
+
+//console.log(item.key+" : "+item.values.length)
+	
+	
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
+		db.run("CREATE TABLE IF NOT EXISTS data2 (item TEXT,keyNo TEXT,countNo INT,keyOpen TEXT,countOpen INT)");
+		var statement = db.prepare("INSERT INTO data2 VALUES (?,?,?,?,?)");
+
+
+if(item.values.length==2){
+	statement.run(item.key,item.values[0].key,item.values[0].value.count,item.values[1].key,item.values[1].value.count); 
+	//console.log(2)
+}
+else {	
+	if(item.values[0].key==""){
+		//console.log("nathing")
+		statement.run(item.key,item.values[0].key,item.values[0].value.count,"no key","no data");
+	}
+	if(item.values[0].key=="open"){
+		//console.log("open")
+		statement.run(item.key,"no key","no data",item.values[0].key,item.values[0].value.count);
+	}
+}
+		
+	
+		statement.finalize();
 	});
+	
+})						
+
+						
+
+						
+						
+});		
+
+///////////////////////////////
+					console.log("STOP");
+				     }
+				}, 5000);
+		}		
+							
+		})
+		.catch( function (error) {
+		console.log("error")
+		piv ();
+		});   
+		
+		
+			
+
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
-
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
-
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-
-		callback(body);
-	});
-}
-
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
-
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-
-		readRows(db);
-
-		db.close();
-	});
-}
-
-initDatabase(run);
+piv ();	
+ 
+   
