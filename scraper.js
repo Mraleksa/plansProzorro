d8d68b2facfd446dd34a3281e7abb087ffc2ab30
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-16 17:38:35 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,7 +7,7 @@ var d3 = require("d3");
 var db = new sqlite3.Database("data.sqlite");
 var p=0; var p2=0;
 
-db.run("DELETE FROM data");
+//db.run("DELETE FROM data");
 
 var currentCount =  "2017-01-01T00:00:00.008329+03:00"
 
@@ -101,71 +101,14 @@ nest.forEach(function(item) {
 db.serialize(function() {
 db.run("CREATE TABLE IF NOT EXISTS data_nest1 (item TEXT,countNo INT,countOpen INT,totalNo INT,totalOpen INT)");
 var statement = db.prepare("INSERT INTO data_nest1 VALUES (?,?,?,?,?)");
-
-if(item.values.length==2){
-	statement.run(item.key,item.values[0].value.count,item.values[1].value.count,item.values[0].value.total,item.values[1].value.total); 
-	//console.log(2)
-}
-else {	
-	if(item.values[0].key==""){
-		//console.log("nathing")
-		statement.run(item.key,item.values[0].value.count,0,item.values[0].value.total,0);
-	}
-	if(item.values[0].key=="open"){
-		//console.log("open")
-		statement.run(item.key,0,item.values[0].value.count,0,item.values[0].value.total);
-	}
-}
+statement.run(item.key,item.values[0].value.count,item.values[1].value.count,item.values[0].value.total,item.values[1].value.total); 
 statement.finalize();
 });//db
-
-	
 	
 })//nest						
 
-
-	
-
-db.run("DELETE FROM data_nest2");	
-	
-exporter.json('SELECT * FROM data_nest1', function (err, json1) {
-var nest1=d3.nest()
- .key(function(d) {return d.item;})
-.rollup(function(v) { return {
-	countNo: d3.sum(v, function(d) { return d.countNo; }),
-	countOpen: d3.sum(v, function(d) { return d.countOpen; }),
-	totalNo: d3.sum(v, function(d) { return d.totalNo; }),
-	totalOpen: d3.sum(v, function(d) { return d.totalOpen; })
-}; })
-.entries(JSON.parse(json1));
-//console.log(nest1[0].value.totalOpen)
-	
-nest1.forEach(function(item) {
-		
-db.serialize(function() {
-db.run("CREATE TABLE IF NOT EXISTS data_nest2 (item TEXT,countNo INT,countOpen INT,totalNo INT,totalOpen INT)");
-var statement = db.prepare("INSERT INTO data_nest2 VALUES (?,?,?,?,?)");
-statement.run(item.key,item.value.countNo,item.value.countOpen,item.value.totalNo,item.value.totalOpen);
-statement.finalize();
-});//db
-})//nest			
-})//exporter2		
-	
-	
-	
-	
-	
-	
 });//exporter		
 
-					
-
-	
-
-
-			
-	
-					
 					
 					
 ///////////////////////////////////////////////////////////////////////////////////////////////////
