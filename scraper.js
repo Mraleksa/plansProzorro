8ea43d7649b541599858d33cac0b7aa12e6561be
Mraleksa <a.gribanovskiy@gmail.com>
2017-06-16 19:30:39 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -106,14 +106,28 @@ db.serialize(function() {
 db.run("CREATE TABLE IF NOT EXISTS data_nest1 (item TEXT,countNo INT,countOpen INT,totalNo INT,totalOpen INT)");
 var statement = db.prepare("INSERT INTO data_nest1 VALUES (?,?,?,?,?)");
 
-if(item.values[0].key==""){
-statement.run(item.key,item.values[0].value.count,item.values[0].value.total,item.values[1].value.count,item.values[1].value.total); 
-console.log("open")
+if(item.values.length==2){
+	if(item.values[0].key==""){
+	statement.run(item.key,item.values[0].value.count,item.values[0].value.total,item.values[1].value.count,item.values[1].value.total); 
+	console.log("open")
+	}
+	if(item.values[0].key=="open"){
+	statement.run(item.key,item.values[1].value.count,item.values[1].value.total,item.values[0].value.count,item.values[0].value.total);
+	console.log("no")
+	}
+	
 }
-if(item.values[0].key=="open"){
-statement.run(item.key,item.values[1].value.count,item.values[1].value.total,item.values[0].value.count,item.values[0].value.total);
-console.log("no")
+else {
+	if(item.values[0].key==""){
+		//console.log("nathing")
+		statement.run(item.key,item.values[0].value.count,item.values[0].value.total,0,0);
+	}
+	if(item.values[0].key=="open"){
+		//console.log("open")
+		statement.run(item.key,0,0,item.values[0].value.count,item.values[0].value.total);
+	}
 }
+
 statement.finalize();
 });//db
 	
